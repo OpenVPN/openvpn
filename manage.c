7e6f2d6f5018e55ba21a808fc9432b31ecfdb4d2
james <james@e7ae566f-a301-0410-adde-c780ea21d3b5>
2009-10-01 20:55:37 +0000

    Minor fix: management interface shouldn't echo 'load-stats' commands to log file.
    git-svn-id: http://svn.openvpn.net/projects/openvpn/branches/BETA21/openvpn@5020 e7ae566f-a301-0410-adde-c780ea21d3b5

--- manage.c
+++ manage.c
@@ -1601,7 +1601,7 @@ man_process_command (struct management *man, const char *line)
       nparms = parse_line (line, parms, MAX_PARMS, "TCP", 0, M_CLIENT, &gc);
       if (parms[0] && streq (parms[0], "password"))
 	msg (D_MANAGEMENT_DEBUG, "MANAGEMENT: CMD 'password [...]'");
-      else
+      else if (!streq (line, "load-stats"))
 	msg (D_MANAGEMENT_DEBUG, "MANAGEMENT: CMD '%s'", line);
 
 #if 0
