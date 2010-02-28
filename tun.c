dea530bcc1c051126d07a382521afe9b0714ec5c
Gert Doering <gert@greenie.muc.de>
2010-02-28 23:09:40 +0100

    remove duplicate code in FREEBSD+DRAGONFLY system-dependent ifconfig (ACKed by Eric F Crist and David Sommerseth)
    (cherry picked from commit dd66b12647852e3f1267be70b0fb3b11deedf377)
    
    Signed-off-by: David Sommerseth <dazo@users.sourceforge.net>
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- tun.c
+++ tun.c
@@ -860,26 +860,15 @@ do_ifconfig (struct tuntap *tt,
 			  ifconfig_remote_netmask,
 			  tun_mtu
 			  );
-      else {
-	if (tt->topology == TOP_SUBNET)
-            argv_printf (&argv,
-                              "%s %s %s netmask %s mtu %d up",
+      else
+	argv_printf (&argv,
+		      "%s %s %s netmask %s mtu %d up",
                               IFCONFIG_PATH,
                               actual,
                               ifconfig_local,
                               ifconfig_remote_netmask,
                               tun_mtu
                               );
-	else
-  	    argv_printf (&argv,
-			  "%s %s %s netmask %s mtu %d up",
-			  IFCONFIG_PATH,
-			  actual,
-			  ifconfig_local,
-			  ifconfig_remote_netmask,
-			  tun_mtu
-			  );
-      }
 	
       argv_msg (M_INFO, &argv);
       openvpn_execve_check (&argv, es, S_FATAL, "FreeBSD ifconfig failed");
