3b44aa49400647b785721e77da9c5e38eeb9fd83
Samuli Seppänen <samuli@openvpn.net>
2011-02-11 16:03:31 +0200

    Added support for viewing config-win32.h paramters to win/show.py
    The win/show.py tools is used to view build parameters interactively. This
    changes it so that it displays parameters parsed from config-win32.h in addition
    to those from win/settings.in.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>
    Acked-by: James Yonan <james@openvpn.net>
    Signed-off-by: David Sommerseth <dazo@users.sourceforge.net>

--- win/show.py
+++ win/show.py
@@ -1,9 +1,9 @@
-from wb import get_config
+from wb import get_config, get_build_params
 from js import JSON
 
 def main():
-    kv = get_config()
-    print JSON().encode(kv)
+    print JSON().encode(get_config())
+    print JSON().encode(get_build_params())
 
 # if we are run directly, and not loaded as a module
 if __name__ == "__main__":
