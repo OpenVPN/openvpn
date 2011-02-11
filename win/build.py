5e0b636ba6cb26a69ce5e863526e44d0fbd33ffc
Samuli Seppänen <samuli@openvpn.net>
2011-02-11 16:20:43 +0200

    Added openvpnserv.exe building to win/build.py
    Made win/build.py call nmake in service-win32 directory to build openvpnserv.exe
    after main build (openvpn.exe) has finished.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>
    Acked-by: James Yonan <james@openvpn.net>
    Signed-off-by: David Sommerseth <dazo@users.sourceforge.net>

--- win/build.py
+++ win/build.py
@@ -1,18 +1,25 @@
 import os, sys
-from wb import system, config, home_fn, cd_home
+from wb import system, config, home_fn, cd_home, cd_service_win32
 
 os.environ['PATH'] += ";%s\\VC" % (os.path.normpath(config['MSVC']),)
 
 def build_vc(cmd):
+    """Make sure environment variables are setup before build"""
     system('cmd /c "vcvarsall.bat x86 && %s"' % (cmd,))
 
 def main():
+    """Build openvpn.exe and openvpnserv.exe"""
     cd_home()
     build_vc("nmake /f %s" % (home_fn('msvc.mak'),))
+    cd_service_win32()
+    build_vc("nmake /f %s" % ('msvc.mak'))
 
 def clean():
+    """Clean up after openvpn.exe and openvpnserv.exe build"""
     cd_home()
     build_vc("nmake /f %s clean" % (home_fn('msvc.mak'),))
+    os.chdir("service-win32")
+    build_vc("nmake /f %s clean" % ('msvc.mak'))
 
 # if we are run directly, and not loaded as a module
 if __name__ == "__main__":
