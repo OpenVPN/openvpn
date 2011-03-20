0df678794a708eb42426ce9844f78ef034dc94bf
James Yonan <james@openvpn.net>
2011-03-20 04:12:26 +0000

    Extended "client-kill" management interface command (server-side) to accept an optional message string.  The message string format is:
      RESTART|HALT,<human-readable-message>
    
    RESTART will tell the client to restart (i.e. SIGUSR1).
    
    HALT will tell the client to exit (i.e. SIGTERM).
    
    On the client, human-readable-message will be communicated via
    management interface:
    
      >NOTIFY,<severity>,<type>,<human-readable-message>"
    
    Version 2.1.3m
    
    
    git-svn-id: http://svn.openvpn.net/projects/openvpn/branches/BETA21/openvpn@7063 e7ae566f-a301-0410-adde-c780ea21d3b5

--- multi.c
+++ multi.c
@@ -2544,13 +2544,14 @@ lookup_by_cid (struct multi_context *m, const unsigned long cid)
 }
 
 static bool
-management_kill_by_cid (void *arg, const unsigned long cid)
+management_kill_by_cid (void *arg, const unsigned long cid, const char *kill_msg)
 {
   struct multi_context *m = (struct multi_context *) arg;
   struct multi_instance *mi = lookup_by_cid (m, cid);
   if (mi)
     {
-      send_restart (&mi->context); /* was: multi_signal_instance (m, mi, SIGTERM); */
+      send_restart (&mi->context, kill_msg); /* was: multi_signal_instance (m, mi, SIGTERM); */
+      multi_schedule_context_wakeup(m, mi);
       return true;
     }
   else
