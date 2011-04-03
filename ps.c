28c8776b65c2300b758cd2b707283a43b4a582e8
James Yonan <james@openvpn.net>
2011-04-03 21:48:15 +0000

    Fixed bug in port-share that could cause port share process to crash with output like this:
    TCP connection established with 85.190.0.3:41781
    85.190.0.3:41781 SIGTERM[soft,port-share-redirect] received, client-instance exiting
    MANAGEMENT: TCP recv error: Socket operation on non-socket
    MANAGEMENT: Client disconnected
    MANAGEMENT: Triggering management exit
    Exiting due to fatal error
    EVENT: epoll_ctl EPOLL_CTL_MOD failed, sd=6: Bad file descriptor (errno=9)
    
    Then an error like this for every incoming connection that should be
    proxied:
    
    76.120.71.74:55302 PORT SHARE: sendmsg failed -- unable to communicate with background process (6,8,-1,-1): Connection refused (errno=111)
    
    Version 2.1.3s
    
    
    git-svn-id: http://svn.openvpn.net/projects/openvpn/branches/BETA21/openvpn@7127 e7ae566f-a301-0410-adde-c780ea21d3b5

--- ps.c
+++ ps.c
@@ -872,6 +872,9 @@ port_share_open (const char *host,
       /* Let msg know that we forked */
       msg_forked ();
 
+      /* Don't interact with management interface */
+      management = NULL;
+
       /* close all parent fds except our socket back to parent */
       close_fds_except (fd[1]);
 
