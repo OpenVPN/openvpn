d313dc950308d960f550279a69f445e712244beb
Alon Bar-Lev <alon.barlev@gmail.com>
2012-02-29 22:11:51 +0200

    build: remove windows specific build system
    It will be completely re-written in future
    
    Signed-off-by: Alon Bar-Lev <alon.barlev@gmail.com>
    Acked-by: Samuli Seppänen <samuli@openvpn.net>
    Signed-off-by: David Sommerseth <davids@redhat.com>

--- win/show.py
+++
@@ -1,9 +0,0 @@
-from wb import get_config
-from js import JSON
-
-def main():
-    print JSON().encode(get_config())
-
-# if we are run directly, and not loaded as a module
-if __name__ == "__main__":
-    main()
