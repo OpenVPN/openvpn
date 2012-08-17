7f696549251d853a9f40a373501b86cc6e5301e3
Heiko Hund <heiko.hund@sophos.com>
2012-08-17 11:51:53 +0200

    remove unused flag SSLF_NO_NAME_REMAPPING
    The flag was used with the --no-name-remapping option that was removed
    in commit 5e86fd93779482b90a191f929edebe414cd78a4f.
    
    Signed-off-by: Heiko Hund <heiko.hund@sophos.com>
    Acked-by: David Sommerseth <davids@redhat.com>
    Message-Id: 1345197113-24636-1-git-send-email-heiko.hund@sophos.com
    URL: http://article.gmane.org/gmane.network.openvpn.devel/6986
    Signed-off-by: David Sommerseth <davids@redhat.com>

--- src/openvpn/ssl_common.h
+++ src/openvpn/ssl_common.h
@@ -288,7 +288,6 @@ struct tls_options
 # define SSLF_CLIENT_CERT_NOT_REQUIRED (1<<0)
 # define SSLF_USERNAME_AS_COMMON_NAME  (1<<1)
 # define SSLF_AUTH_USER_PASS_OPTIONAL  (1<<2)
-# define SSLF_NO_NAME_REMAPPING        (1<<3)
 # define SSLF_OPT_VERIFY               (1<<4)
 # define SSLF_CRL_VERIFY_DIR           (1<<5)
   unsigned int ssl_flags;
