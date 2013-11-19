1146c724e4d966248d23a0b350bc76d1af35a00c
Heiko Hund <heiko.hund@sophos.com>
2013-11-19 18:36:14 +0100

    Support non-ASCII characters in Windows tmp path
    Get the temporary path from the system as UCS-2 and convert it to
    UTF-8 and return that for internal use.
    
    Fix trac#278.
    
    Signed-off-by: Heiko Hund <heiko.hund@sophos.com>
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Message-Id: <1384882574-28242-1-git-send-email-heiko.hund@sophos.com>
    URL: http://article.gmane.org/gmane.network.openvpn.devel/8002
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/win32.c
+++ src/openvpn/win32.c
@@ -996,19 +996,27 @@ set_win_sys_path_via_env (struct env_set *es)
 const char *
 win_get_tempdir()
 {
-  static char buf[MAX_PATH];
-  char *tmpdir = buf;
+  static char tmpdir[MAX_PATH];
+  WCHAR wtmpdir[MAX_PATH];
 
-  CLEAR(buf);
+  if (!GetTempPathW(_countof(wtmpdir), wtmpdir))
+    {
+      /* Warn if we can't find a valid temporary directory, which should
+       * be unlikely.
+       */
+      msg (M_WARN, "Could not find a suitable temporary directory."
+          " (GetTempPath() failed).  Consider using --tmp-dir");
+      return NULL;
+    }
 
-  if (!GetTempPath(sizeof(buf),buf)) {
-    /* Warn if we can't find a valid temporary directory, which should
-     * be unlikely.
-     */
-    msg (M_WARN, "Could not find a suitable temporary directory."
-         " (GetTempPath() failed).  Consider to use --tmp-dir");
-    tmpdir = NULL;
-  }
+  if (WideCharToMultiByte (CP_UTF8, 0, wtmpdir, -1, NULL, 0, NULL, NULL) > sizeof (tmpdir))
+    {
+      msg (M_WARN, "Could not get temporary directory. Path is too long."
+          "  Consider using --tmp-dir");
+      return NULL;
+    }
+
+  WideCharToMultiByte (CP_UTF8, 0, wtmpdir, -1, tmpdir, sizeof (tmpdir), NULL, NULL);
   return tmpdir;
 }
 #endif
