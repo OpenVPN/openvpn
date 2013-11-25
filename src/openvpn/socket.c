aa162d44edae8530391775b55e7b4f149548537e
Arne Schwabe <arne@rfc2549.org>
2013-11-25 13:31:16 +0100

    When resolving fails print the error message from socket layer
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Message-Id: <1385382680-5912-5-git-send-email-arne@rfc2549.org>
    URL: http://article.gmane.org/gmane.network.openvpn.devel/8059
    
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/socket.c
+++ src/openvpn/socket.c
@@ -187,15 +187,15 @@ openvpn_getaddrinfo (unsigned int flags,
       const char *fmt;
       int level = 0;
 
-      fmt = "RESOLVE: Cannot resolve host address: %s: %s";
+      fmt = "RESOLVE: Cannot resolve host address: %s:%s (%s)";
       if ((flags & GETADDR_MENTION_RESOLVE_RETRY)
           && !resolve_retry_seconds)
-        fmt = "RESOLVE: Cannot resolve host address: %s: %s (I would have retried this name query if you had specified the --resolv-retry option.)";
+        fmt = "RESOLVE: Cannot resolve host address: %s:%s (%s) (I would have retried this name query if you had specified the --resolv-retry option.)";
 
       if (!(flags & GETADDR_RESOLVE) || status == EAI_FAIL)
         {
-          msg (msglevel, "RESOLVE: Cannot parse IP address: %s:%s",
-               print_hostname,print_servname);
+          msg (msglevel, "RESOLVE: Cannot parse IP address: %s:%s (%s)",
+               print_hostname,print_servname, gai_strerror(status));
           goto done;
         }
 
@@ -1713,8 +1713,10 @@ phase2_socks_client (struct link_socket *sock, bool *remote_changed,
     sock->did_resolve_remote = false;
 
     addr_zero_host(&sock->info.lsa->actual.dest);
-    if (sock->info.lsa->remote_list)
+    if (sock->info.lsa->remote_list) {
 	freeaddrinfo(sock->info.lsa->remote_list);
+	sock->info.lsa->remote_list=NULL;
+    }
 
     resolve_remote (sock, 1, NULL, signal_received);
 }
