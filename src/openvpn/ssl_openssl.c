25bc9f082432034315b3efd9401aa8a9e5884506
Gert Doering <gert@greenie.muc.de>
2014-04-13 17:29:32 +0200

    Add SSL library version reporting.
    Print the version of the SSL and LZO library (if any) used.
    
    SSL library version is also sent as IV_SSL=<version> to the server if
    --push-peer-info is enabled.
    
    Signed-off-by: Gert Doering <gert@greenie.muc.de>
    Acked-by: Steffan Karger <steffan.karger@fox-it.com>
    Message-Id: <20140416152456.GI16637@greenie.muc.de>
    URL: http://article.gmane.org/gmane.network.openvpn.devel/8537
    (cherry picked from commit 1ec984b154aa3247ef58c9d44e7e477880b632b1)

--- src/openvpn/ssl_openssl.c
+++ src/openvpn/ssl_openssl.c
@@ -1345,4 +1345,10 @@ get_highest_preference_tls_cipher (char *buf, int size)
   SSL_CTX_free (ctx);
 }
 
+char *
+get_ssl_library_version(void)
+{
+    return SSLeay_version(SSLEAY_VERSION);
+}
+
 #endif /* defined(ENABLE_SSL) && defined(ENABLE_CRYPTO_OPENSSL) */
