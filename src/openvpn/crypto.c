d66ba9b212e95cdd1880be58036e62d59e5dbb6a
Steffan Karger <steffan@karger.me>
2014-06-08 17:04:32 +0200

    Improve --show-ciphers to show if a cipher can be used in static key mode
    Also remove the bulky warning from init_key_type() and add the information
    to the --show-ciphers output.
    
    Signed-off-by: Steffan Karger <steffan@karger.me>
    Acked-by: Arne Schwabe <arne@rfc2549.org>
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Message-Id: <53BAEF65.2070509@karger.me>
    URL: http://article.gmane.org/gmane.network.openvpn.devel/8852
    Signed-off-by: Gert Doering <gert@greenie.muc.de>
    (cherry picked from commit d344820faeae987f52e574e15812c86aa5c59ae6)

--- src/openvpn/crypto.c
+++ src/openvpn/crypto.c
@@ -434,11 +434,7 @@ init_key_type (struct key_type *kt, const char *ciphername,
 	      || (cfb_ofb_allowed && cipher_kt_mode_ofb_cfb(kt->cipher))
 #endif
 	      ))
-#ifdef ENABLE_SMALL
 	  msg (M_FATAL, "Cipher '%s' mode not supported", ciphername);
-#else
-	  msg (M_FATAL, "Cipher '%s' uses a mode not supported by " PACKAGE_NAME " in your current configuration.  CBC mode is always supported, while CFB and OFB modes are supported only when using SSL/TLS authentication and key exchange mode, and when " PACKAGE_NAME " has been built with ALLOW_NON_CBC_CIPHERS.", ciphername);
-#endif
       }
     }
   else
