1a7fc1ea2207580693b2801099f8a473f1c07828
Gert Doering <gert@greenie.muc.de>
2015-04-28 13:04:23 +0200

    Print helpful error message on --mktun/--rmtun if not available.
    OpenVPN only supports --mktun/--rmtun to create/destroy persistant
    tunnels on Linux.  On BSD OSes, "ifconfig tun0 create" can do the
    same job, so we do not actually need to support it - but the previous
    error message ("unknown option") wasn't helpful.  So always accept
    the option now, and on non-supported systems, direct user to manpage.
    
    Trac #85
    
    Signed-off-by: Gert Doering <gert@greenie.muc.de>
    Acked-by: David Sommerseth <dazo@users.sourceforge.net>
    Message-Id: <1430219063-12291-1-git-send-email-gert@greenie.muc.de>
    URL: http://article.gmane.org/gmane.network.openvpn.devel/9617
    (cherry picked from commit 4ad2b65d9deb3197d847d7dcc36715aa5394836f)

--- src/openvpn/options.c
+++ src/openvpn/options.c
@@ -7038,7 +7038,6 @@ add_option (struct options *options,
       options->pkcs11_id_management = true;
     }
 #endif
-#ifdef ENABLE_FEATURE_TUN_PERSIST
   else if (streq (p[0], "rmtun"))
     {
       VERIFY_PERMISSION (OPT_P_GENERAL);
@@ -7051,7 +7050,6 @@ add_option (struct options *options,
       options->persist_config = true;
       options->persist_mode = 1;
     }
-#endif
   else if (streq (p[0], "peer-id"))
     {
       VERIFY_PERMISSION (OPT_P_PEER_ID);
