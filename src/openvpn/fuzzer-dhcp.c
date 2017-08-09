519fbf338bf0bb49f547a7658392a87cd2c73cd3
Guido Vranken <guidovranken@gmail.com>
2017-08-09 16:23:48 +0200

    DHCP fuzzer: add comments

--- src/openvpn/fuzzer-dhcp.c
+++ src/openvpn/fuzzer-dhcp.c
@@ -6,21 +6,28 @@ int LLVMFuzzerInitialize(int *argc, char ***argv)
 {
     return 1;
 }
+
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     struct gc_arena gc;
     struct buffer ipbuf;
     in_addr_t ret;
 
+    /* Encapsulate raw input buffer in a struct buffer */
     ipbuf = alloc_buf(size);
     if ( buf_write(&ipbuf, data, size) == false ) {
         goto cleanup;
     }
+
+    /* Change internal buffer state to test whether dhcp_extract_router_msg
+     * depends on a certain state */
     fuzzer_alter_buffer(&ipbuf);
+
     ret = dhcp_extract_router_msg(&ipbuf);
 #ifdef MSAN
     test_undefined_memory(&ret, sizeof(ret));
 #endif
+
 cleanup:
     free_buf(&ipbuf);
 
