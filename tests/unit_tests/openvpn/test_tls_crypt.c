d47228e71de6cbbf860746a50a3ecf8025e35653
Steffan Karger <steffan.karger@fox-it.com>
2017-08-12 11:53:52 +0200

    tls-crypt: introduce tls_crypt_kt()
    Reduces code duplication (and prepares for tls-crypt-v2, which needs the
    same functionality at more places).
    
    Because tls_crypt_kt() is a static function we now need to include
    tls_crypt.c from the tests, rather than tls_crypt.h.
    
    Signed-off-by: Steffan Karger <steffan.karger@fox-it.com>
    Acked-by: Antonio Quartulli <a@unstable.cc>
    Message-Id: <1502531632-16833-1-git-send-email-steffan.karger@fox-it.com>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg15229.html
    Signed-off-by: David Sommerseth <davids@openvpn.net>
    (cherry picked from commit 489c7bf93ec618e03dbd9618efbb6e251a65e76c)

--- tests/unit_tests/openvpn/test_tls_crypt.c
+++ tests/unit_tests/openvpn/test_tls_crypt.c
@@ -39,7 +39,7 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-#include "tls_crypt.h"
+#include "tls_crypt.c"
 
 #include "mock_msg.h"
 
@@ -60,23 +60,13 @@ setup(void **state) {
     struct test_context *ctx = calloc(1, sizeof(*ctx));
     *state = ctx;
 
-    ctx->kt.cipher = cipher_kt_get("AES-256-CTR");
-    ctx->kt.digest = md_kt_get("SHA256");
-    if (!ctx->kt.cipher)
-    {
-        printf("No AES-256-CTR support, skipping test.\n");
-        return 0;
-    }
-    if (!ctx->kt.digest)
-    {
-        printf("No HMAC-SHA256 support, skipping test.\n");
-        return 0;
-    }
-    ctx->kt.cipher_length = cipher_kt_key_size(ctx->kt.cipher);
-    ctx->kt.hmac_length = md_kt_size(ctx->kt.digest);
-
     struct key key = { 0 };
 
+    ctx->kt = tls_crypt_kt();
+    if (!ctx->kt.cipher || !ctx->kt.digest)
+    {
+        return 0;
+    }
     init_key_ctx(&ctx->co.key_ctx_bi.encrypt, &key, &ctx->kt, true, "TEST");
     init_key_ctx(&ctx->co.key_ctx_bi.decrypt, &key, &ctx->kt, false, "TEST");
 
