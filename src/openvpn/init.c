868b200c3aef6ee5acfdf679770832018ebc7b70
Arne Schwabe <arne@rfc2549.org>
2020-02-17 15:43:35 +0100

    Only announce IV_NCP=2 when we are willing to support these ciphers
    We currently always announce IV_NCP=2 when we support these ciphers even
    when we do not accept them. This lead to a server pushing a AES-GCM-128
    cipher to clients and the client then rejecting it.
    
    Patch V2:  Remove unecessary restoring of ncp_ciphers
    Patch V3:  Do not add ncp_ciphers in context
    
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>
    Acked-by: Lev Stipakov <lstipakov@gmail.com>
    Message-Id: <20200217144339.3273-2-arne@rfc2549.org>
    URL: https://www.mail-archive.com/search?l=mid&q=20200217144339.3273-2-arne@rfc2549.org
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/init.c
+++ src/openvpn/init.c
@@ -2827,6 +2827,7 @@ do_init_crypto_tls(struct context *c, const unsigned int flags)
     to.tcp_mode = link_socket_proto_connection_oriented(options->ce.proto);
     to.config_ciphername = c->c1.ciphername;
     to.config_authname = c->c1.authname;
+    to.config_ncp_ciphers = options->ncp_ciphers;
     to.ncp_enabled = options->ncp_enabled;
     to.transition_window = options->transition_window;
     to.handshake_window = options->handshake_window;
--- src/openvpn/ssl_common.h
+++ src/openvpn/ssl_common.h
@@ -290,6 +290,7 @@ struct tls_options
 
     const char *config_ciphername;
     const char *config_authname;
+    const char *config_ncp_ciphers;
     bool ncp_enabled;
 
     bool tls_crypt_v2;
