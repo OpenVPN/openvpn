769c894db29f76f6510569fc20633fb6f0c357f9
Gert Doering <gert@greenie.muc.de>
2020-09-08 12:51:30 +0200

    Fix error detection / abort in --inetd corner case.
    Calling "openvpn --inetd" from the CLI (= no socket on stdin) will
    lead to endless looping in the accept(4) loop.
    
    Instead of cluttering that function further, detect failure to call
    getsockame() in phase2_inetd() already, and trigger a M_FATAL abort
    on "errno == ENOTSOCK" ("The argument s is a file, not a socket").
    
    While at it, uncrustify the --bind-dev code (whitespace only).
    
    Trac: #350
    
    Signed-off-by: Gert Doering <gert@greenie.muc.de>
    Acked-by: Arne Schwabe <arne@rfc2549.org>
    Message-Id: <20200908105130.24171-1-gert@greenie.muc.de>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg20897.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>
    (cherry picked from commit a09a2fadbadb5dc435f6fccc581163e1f637f43f)

--- src/openvpn/socket.c
+++ src/openvpn/socket.c
@@ -1995,8 +1995,14 @@ phase2_inetd(struct link_socket *sock, const struct frame *frame,
             }
             else
             {
-                msg(M_WARN, "inetd(%s): getsockname(%d) failed, using AF_INET",
+                int saved_errno = errno;
+                msg(M_WARN|M_ERRNO, "inetd(%s): getsockname(%d) failed, using AF_INET",
                     proto2ascii(sock->info.proto, sock->info.af, false), (int)sock->sd);
+                /* if not called with a socket on stdin, --inetd cannot work */
+                if (saved_errno == ENOTSOCK)
+                {
+                    msg(M_FATAL, "ERROR: socket required for --inetd operation");
+                }
             }
         }
 #else  /* ifdef HAVE_GETSOCKNAME */
@@ -2012,7 +2018,6 @@ phase2_inetd(struct link_socket *sock, const struct frame *frame,
                                  false,
                                  sock->inetd == INETD_NOWAIT,
                                  signal_received);
-
     }
     ASSERT(!remote_changed);
 }
