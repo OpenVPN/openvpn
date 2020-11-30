fb789947ab1eba3e68fb8e4b3551d095a53962bd
Arne Schwabe <arne@rfc2549.org>
2020-11-30 13:39:28 +0100

    Fix auth-token not being updated if auth-nocache is set
    This fixes the auth-token not being updated if auth-nocache is set. Our
    set_auth_token method ensures that the auth-token always has a username
    but is a little bit too strict in the check.
    
    Also add doxygen documentation and remove null checks. We use this function
    only with non-null pointers and it makes it a bit nicer to read.
    
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Message-Id: <20201130123928.21837-1-arne@rfc2549.org>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg21291.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/misc.h
+++ src/openvpn/misc.h
@@ -145,6 +145,17 @@ void fail_user_pass(const char *prefix,
 
 void purge_user_pass(struct user_pass *up, const bool force);
 
+/**
+ * Sets the auth-token to token if a username is available from either
+ * up or already present in tk. The method will also purge up if
+ * the auth-nocache option is active.
+ *
+ * @param up        (non Auth-token) Username/password
+ * @param tk        auth-token userpass to set
+ * @param token     token to use as password for the
+ *
+ * @note    all parameters to this function must not be null.
+ */
 void set_auth_token(struct user_pass *up, struct user_pass *tk,
                     const char *token);
 
