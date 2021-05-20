716049923e3e70c3de938d6da5d05f529ec515b5
Arne Schwabe <arne@rfc2549.org>
2021-05-20 17:11:40 +0200

    Move auth_token_state from multi to key_state
    The auth-token check is tied to the username/password that is coming
    via a specific SSL session, so keep the state also in the key_state
    structure.
    
    This also ensures the auth_token_state is always set to 0 on a new
    session since we clear the key_state object at the start of a new
    SSL session.
    
    This is a prerequisite patch to fix 2020-15078 in the following two
    commits.
    
    This also applies the changes to the auth_token_test.c. The change of
    tls_session to a pointer is necessary since before that we had tls_session
    not tied to the multi and had two tls_session used in the test. One
    implicitly in tls_multi and one explicit one. Merge these to one.
    
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>
    Acked-by: Antonio Quartulli <antonio@openvpn.net>
    Message-Id: <20210520151148.2565578-1-arne@rfc2549.org>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg22415.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/ssl_common.h
+++ src/openvpn/ssl_common.h
@@ -182,6 +182,8 @@ enum auth_deferred_result {
 struct key_state
 {
     int state;
+    /** The state of the auth-token sent from the client */
+    int auth_token_state_flags;
 
     /**
      * Key id for this key_state,  inherited from struct tls_session.
@@ -598,8 +600,6 @@ struct tls_multi
      * OpenVPN 3 clients sometimes wipes or replaces the username with a
      * username hint from their config.
      */
-    int auth_token_state_flags;
-    /**< The state of the auth-token sent from the client last time */
 
     /* For P_DATA_V2 */
     uint32_t peer_id;
