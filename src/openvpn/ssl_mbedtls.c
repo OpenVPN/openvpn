ccee09d1478aa69783926c208bfa235dcb055124
Max Fillinger <maximilian.fillinger@foxcrypto.com>
2021-07-01 19:14:58 +0200

    Update Fox e-mail address in copyright notices
    Replace openvpn@fox-it.com with openvpn@foxcrypto.com.
    
    Signed-off-by: Max Fillinger <maximilian.fillinger@foxcrypto.com>
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Message-Id: <20210701171458.8897-1-maximilian.fillinger@foxcrypto.com>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg22608.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/ssl_mbedtls.c
+++ src/openvpn/ssl_mbedtls.c
@@ -6,7 +6,7 @@
  *             packet compression.
  *
  *  Copyright (C) 2002-2021 OpenVPN Inc <sales@openvpn.net>
- *  Copyright (C) 2010-2021 Fox Crypto B.V. <openvpn@fox-it.com>
+ *  Copyright (C) 2010-2021 Fox Crypto B.V. <openvpn@foxcrypto.com>
  *  Copyright (C) 2006-2010, Brainspark B.V.
  *
  *  This program is free software; you can redistribute it and/or modify
