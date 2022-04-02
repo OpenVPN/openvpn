c0fa37eda2a12c6587e9b2db2c0a33c91d3ced6e
Arne Schwabe <arne@rfc2549.org>
2022-04-02 09:08:56 +0200

    networking: remove duplicate methods from networking_sitnl.c
    The net_ctx_init/reset/free methods of sitnl are the same dummy
    methods that are already defined for non-Linux platforms in the
    networking.h header.
    
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>
    Signed-off-by: Antonio Quartulli <a@unstable.cc>
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Message-Id: <20220402070902.30282-2-a@unstable.cc>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg24054.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/networking.h
+++ src/openvpn/networking.h
@@ -33,7 +33,12 @@ struct context;
 /* define mock types to ensure code builds on any platform */
 typedef void *openvpn_net_ctx_t;
 typedef void *openvpn_net_iface_t;
+#endif /* ifdef ENABLE_SITNL */
 
+/* Only the iproute2 backend implements these functions,
+ * the rest can rely on these stubs
+ */
+#if !defined(ENABLE_IPROUTE)
 static inline int
 net_ctx_init(struct context *c, openvpn_net_ctx_t *ctx)
 {
@@ -51,7 +56,7 @@ net_ctx_free(openvpn_net_ctx_t *ctx)
 {
     (void)ctx;
 }
-#endif /* ifdef ENABLE_SITNL */
+#endif /* !defined(ENABLE_IPROUTE) */
 
 #if defined(ENABLE_SITNL) || defined(ENABLE_IPROUTE)
 
