e0dbe5c4e290c0c24a26a9e5c19957ebaca09543
Arne Schwabe <arne@rfc2549.org>
2022-08-17 15:53:48 +0200

    Fix IV_PLAT_VER and UV_ variables sent without push-peer-info
    Commit 8c72d7981 changed the push_peer_info_detail to have an
    additional level for P2P NCP and shifting most of the other levels
    with 1. The check for UV_ and IV_PLAT_VER was not changed accordingly.
    
    Fixes: 8c72d7981 ("Support NCP in pure P2P VPN setups")
    
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>
    Acked-by: Antonio Quartulli <a@unstable.cc>
    Message-Id: <20220817135348.844178-1-arne@rfc2549.org>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg24956.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/ssl.c
+++ src/openvpn/ssl.c
@@ -2036,7 +2036,7 @@ push_peer_info(struct buffer *buf, struct tls_session *session)
                 {
                     if ((((strncmp(e->string, "UV_", 3) == 0
                            || strncmp(e->string, "IV_PLAT_VER=", sizeof("IV_PLAT_VER=") - 1) == 0)
-                          && session->opt->push_peer_info_detail >= 2)
+                          && session->opt->push_peer_info_detail > 2)
                          || (strncmp(e->string, "IV_GUI_VER=", sizeof("IV_GUI_VER=") - 1) == 0)
                          || (strncmp(e->string, "IV_SSO=", sizeof("IV_SSO=") - 1) == 0)
                          )
