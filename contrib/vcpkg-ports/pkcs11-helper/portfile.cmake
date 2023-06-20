e8881ec6dd63bd80ce05202573eac54ab8657fcb
Frank Lichtenheld <frank@lichtenheld.com>
2023-06-20 15:53:07 +0200

    CMake: Add complete MinGW and MSVC build
    This is based on the initial CMake patch by
    Arne Schwabe, but extends that to provide
    a complete replacement for existing MinGW
    build (autotools based) and MSVC build
    (openvpn.sln).
    
    The following features are added while switching
    these builds to CMake:
     - vcpkg support for MinGW build, allowing for
       trivial cross-compilation on Linux
     - Add unittests to MSVC build
     - Rework MSVC config header generation, removing
       need for separate headers between autotools
       and MSVC
    
    The following advantages are reasons for switching
    to CMake over the existing MSVC build:
     - Easier to maintain CMake files without IDE
       than the sln and vcxproj files
     - Able to maintain MSVC and MinGW build side-by-side
    
    The plan is to completely remove the existing MSVC
    build system but leave the existing autotools builds
    in place as-is, including MinGW support.
    
    CMake is not the intended build system for Unix-like
    platforms and there are no current plans to switch
    to it.
    
    v2:
     - Reduce default warning level for MSVC to /W2. With
       /W3 the build is just much too noisy, making it
       difficult to spot new warnings.
     - Change MSVC CMake presets to have hardcoded build
       type. When using pkg_search_module MSVC Multi-Config
       builds do not work correctly at all since PkgConfig
       doesn't seem to be able to create multi-config
       libraries like find_package does.
     - Change minGW presets to be Multi-Config capable.
     - Remove OPENVPN_VERSION_MAJOR, OPENVPN_VERSION_MINOR,
       OPENVPN_VERSION_PATCH from config.h.cmake.in.
       They are not required and cause macro redefinition
       warnings in MSVC (with openvpn-plugin.h). gcc doesn't
       warn about this because the definitions are identical
       so no need to fix this in autoheader config.h.in.
    v3:
     - Apply fixes by Lev Stipakov to match MSVC compile
       options better to previous build.
     - Apply change by Lev Stipakov to enable generation
       of PDB files.
     - Move /Brepro to its own commit. This is a behavior
       change that should be more visible.
     - Rebase on top of my dist fixes.
    
    Change-Id: I237f28eca618d4fc476225b887c0be26cca362b1
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>
    Acked-by: Lev Stipakov <lstipakov@gmail.com>
    Message-Id: <20230620135310.94455-3-frank@lichtenheld.com>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg26754.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- contrib/vcpkg-ports/pkcs11-helper/portfile.cmake
+++ contrib/vcpkg-ports/pkcs11-helper/portfile.cmake
@@ -28,11 +28,27 @@ if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
         OPENSSL_HOME=${CURRENT_PACKAGES_DIR}/../openssl_${TARGET_TRIPLET}
   )
 
-  file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/pkcs11-helper.dll.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-  file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/pkcs11-helper.dll.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+  file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/pkcs11-helper.dll.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib RENAME pkcs11-helper.lib)
+  file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/pkcs11-helper.dll.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib RENAME pkcs11-helper.lib)
 
   file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/libpkcs11-helper-1.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
   file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/libpkcs11-helper-1.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+
+  set(PACKAGE_VERSION "${VERSION}")
+    set(libdir [[${prefix}/lib]])
+    set(exec_prefix [[${prefix}]])
+    set(PKCS11H_FEATURES key_prompt openssl engine_crypto_cryptoapi engine_crypto_openssl debug threading token data certificate slotevent engine_crypto)
+    set(LIBS -lkernel32 -luser32 -lgdi32 -lwinspool -lshell32 -lole32 -loleaut32 -luuid -lcomdlg32 -ladvapi32)
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        set(includedir [[${prefix}/include]])
+        set(outfile "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libpkcs11-helper-1.pc")
+        configure_file("${SOURCE_PATH}/lib/libpkcs11-helper-1.pc.in" "${outfile}" @ONLY)
+    endif()
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        set(includedir [[${prefix}/../include]])
+        set(outfile "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libpkcs11-helper-1.pc")
+        configure_file("${SOURCE_PATH}/lib/libpkcs11-helper-1.pc.in" "${outfile}" @ONLY)
+    endif()
 
   file(INSTALL ${SOURCE_PATH}/include/pkcs11-helper-1.0 DESTINATION ${CURRENT_PACKAGES_DIR}/include/)
 
@@ -45,11 +61,11 @@ else()
     --disable-crypto-engine-polarssl --disable-crypto-engine-mbedtls
     )
   vcpkg_install_make()
-  vcpkg_fixup_pkgconfig()
 
   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 endif()
 
+vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
