ede590e57c96c2b16d9bf462c4b1dd967b37c432
Arne Schwabe <arne@rfc2549.org>
2023-07-12 11:54:12 +0200

    Mock openvpn_exece on win32 also for test_tls_crypt
    This function is needed to commpile on win32 as run_command.c defines it
    on Unix Linux but on windows it is defined in win32.c which pulls in too
    many other unresolvable symbols.
    
    Patch v2: Also add mock_win32_execve.c to automake files
    
    Change-Id: I8c8fe298eb30e211279f3fc010584b9d3bc14b4a
    Signed-off-by: Arne Schwabe <arne@rfc2549.org>
    Acked-by: Frank Lichtenheld <frank@lichtenheld.com>
    Message-Id: <20230712095412.570106-1-arne@rfc2549.org>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg26849.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

---
+++ tests/unit_tests/openvpn/mock_win32_execve.c
@@ -0,0 +1,37 @@
+/*
+ *  OpenVPN -- An application to securely tunnel IP networks
+ *             over a single TCP/UDP port, with support for SSL/TLS-based
+ *             session authentication and key exchange,
+ *             packet encryption, packet authentication, and
+ *             packet compression.
+ *
+ *  Copyright (C) 2023 OpenVPN Inc <sales@openvpn.net>
+ *  Copyright (C) 2023 Arne Schwabe <arne@rfc2549.org>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "config.h"
+#include "syshead.h"
+
+#include "win32.h"
+
+#ifdef _WIN32
+int
+openvpn_execve(const struct argv *a, const struct env_set *es, const unsigned int flags)
+{
+    ASSERT(0);
+}
+#endif
