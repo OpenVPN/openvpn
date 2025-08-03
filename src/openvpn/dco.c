ad73d827d337ae296e4f2fd57e78e37856091c8c
Frank Lichtenheld <frank@lichtenheld.com>
2025-08-03 16:51:20 +0200

    Update GPL header in all source files to current recommended version
    This removes the postal address of the FSF and replaces
    it with their URL.
    
    Mostly generated with
    sed -i -e 's@if not, write to the Free Software Foundation, Inc.,\
                @if not, see <https://www.gnu.org/licenses/>.@'
    sed -i -e '/51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA/d'
    sed -i -e '/59 Temple Place, Suite 330, Boston, MA  02111-1307  USA/d'
    
    With some manual fix-ups afterwards.
    
    Change-Id: Ic3959970fa9ab993e98d4b38c025fd0efc7b92f2
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Message-Id: <20250803145126.23494-1-gert@greenie.muc.de>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg32481.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/dco.c
+++ src/openvpn/dco.c
@@ -20,8 +20,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program (see the file COPYING included with this
- *  distribution); if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  distribution); if not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifdef HAVE_CONFIG_H
--- src/openvpn/dco_linux.h
+++ src/openvpn/dco_linux.h
@@ -16,8 +16,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program (see the file COPYING included with this
- *  distribution); if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  distribution); if not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef DCO_LINUX_H
 #define DCO_LINUX_H
