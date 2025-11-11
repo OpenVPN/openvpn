4fa8539a088e490633e0f7114c21e03d61871dd6
Frank Lichtenheld <frank@lichtenheld.com>
2025-11-11 16:32:24 +0100

    openssl_compat: Avoid conversion warning for SSL_get_negotiated_group
    SSL_get_negotiated_group is documented to return
    int and SSL_group_to_name definitely expects an int.
    
    But SSL_get_negotiated_group is actually a macro
    implemented by SSL_ctrl, which does return a long.
    So to avoid the conversion warning we need the cast.
    
    Change-Id: I31024f93d9d9d0f678fb39d4758a7e870bf00873
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>
    Acked-by: Arne Schwabe <arne-openvpn@rfc2549.org>
    Gerrit URL: https://gerrit.openvpn.net/c/openvpn/+/1359
    Message-Id: <20251111153230.29865-1-gert@greenie.muc.de>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg34316.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/openssl_compat.h
+++ src/openvpn/openssl_compat.h
@@ -194,21 +194,12 @@ SSL_get0_peer_signature_name(const SSL *ssl, const char **sigalg)
           LIBRESSL_VERSION_NUMBER > 0x3050400fL) */
 
 #if OPENSSL_VERSION_NUMBER < 0x30200000L && OPENSSL_VERSION_NUMBER >= 0x30000000L
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wconversion"
-#endif
-
 static inline const char *
 SSL_get0_group_name(SSL *s)
 {
-    int nid = SSL_get_negotiated_group(s);
+    int nid = (int)SSL_get_negotiated_group(s);
     return SSL_group_to_name(s, nid);
 }
-
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
 #endif
 
 /* Introduced in OpenSSL 3.6.0 */
