eb0666c8dcd7a09528594d1ce172aadb3be2d0cf
Frank Lichtenheld <frank@lichtenheld.com>
2025-11-16 14:59:04 +0100

    interval: Fix conversion warning
    Use correct return type of the function.
    
    Change-Id: I445b2e2cd14e55cc1ae3efdf1650b28e18992aa5
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>
    Acked-by: Gert Doering <gert@greenie.muc.de>
    Gerrit URL: https://gerrit.openvpn.net/c/openvpn/+/1291
    Message-Id: <20251116135909.15964-1-gert@greenie.muc.de>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg34464.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- src/openvpn/interval.c
+++ src/openvpn/interval.c
@@ -38,11 +38,6 @@ interval_init(struct interval *top, int horizon, int refresh)
     top->horizon = horizon;
 }
 
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wconversion"
-#endif
-
 bool
 event_timeout_trigger(struct event_timeout *et, struct timeval *tv, const int et_const_retry)
 {
@@ -52,7 +47,7 @@ event_timeout_trigger(struct event_timeout *et, struct timeval *tv, const int et
     }
 
     bool ret = false;
-    time_t wakeup = event_timeout_remaining(et);
+    interval_t wakeup = event_timeout_remaining(et);
 
     if (wakeup <= 0)
     {
@@ -82,7 +77,3 @@ event_timeout_trigger(struct event_timeout *et, struct timeval *tv, const int et
     }
     return ret;
 }
-
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic pop
-#endif
