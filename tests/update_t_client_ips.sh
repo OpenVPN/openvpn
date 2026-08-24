dc162485236b70ecfe1be639dbd9db8244a85d54
Frank Lichtenheld <frank@lichtenheld.com>
2026-08-24 20:03:47 +0200

    t_client.sh.in: Allow to override the location of t_client_ips.rc
    Makes it easier to use t_client.sh in other contexts.
    
    This is based on work by Gert Doering for the t_server
    test infrastructure.
    
    Change-Id: Ia4a9a6bafb6bcb3fe331a92cfbb64be78f6aa33a
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>
    Acked-by: Razvan Cojocaru <razvanc@mailbox.org>
    Gerrit URL: https://gerrit.openvpn.net/c/openvpn/+/1756
    Message-Id: <20260824180353.12076-1-gert@greenie.muc.de>
    URL: https://www.mail-archive.com/openvpn-devel@lists.sourceforge.net/msg38649.html
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- tests/update_t_client_ips.sh
+++ tests/update_t_client_ips.sh
@@ -3,8 +3,6 @@
 # This --up script caches the IPs handed out by the test VPN server to a file
 # for later use.
 
-RC="$TOP_BUILDDIR/t_client_ips.rc"
-
 grep EXPECT_IFCONFIG4_$TESTNUM= $RC >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "EXPECT_IFCONFIG4_$TESTNUM=$ifconfig_local" >>$RC
